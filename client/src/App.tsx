8297ac0b1f11fb685f3b5f6a6c8a24076eca3b45
WHL99 <xjv298347@gmail.com>
2023-03-14 15:06:19 +0100

    Change file structure

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,37 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
+import { Note } from './models/note'
 
 function App() {
+  const [notes, setNotes] = useState<Note[]>([])
+
+  const getNotes = async () => {
+    try {
+      const response = await fetch('http://localhost:5005/api/notes/', { method: 'GET' })
+      const notes = await response.json()
+      setNotes(notes)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  useEffect(() => {
+    getNotes()
+  }, [])
+
   return (
     <div className='App'>
       <h1>Hello world!</h1>
+      {JSON.stringify(notes)}
+      {/* {notes &&
+        notes.map((note) => {
+          return (
+            <div>
+              <h1>{note.title}</h1>
+              <p>{note.text}</p>
+            </div>
+          )
+        })} */}
     </div>
   )
 }
